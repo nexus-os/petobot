2cffc3e22ea45912e5baee009a4766f5c90c88c8
Simone Baratta <Conte91@gmail.com>
2024-12-28 14:01:39 +0100

    PR

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,17 +5,45 @@
 
 // BLE UUIDs
 #define SERVICE_UUID           "08daa714-ccf1-42a8-8a88-535652d04bac" // UUID for the BLE service
-#define CHARACTERISTIC_UUID_LEFT  "1EF71EF7-1EF7-1EF7-1EF7-1EF71EF71EF7" // UUID for X
-#define CHARACTERISTIC_UUID_RIGHT  "1E551EF7-1E55-1E55-1E55-1E551E551EF7" // UUID for Y
+#define CHARACTERISTIC_UUID_GO  "FA57FA57-FA57-FA57-FA57-FA57FA57FA57" // UUID to start
+#define CHARACTERISTIC_UUID_STOP  "1E55FA57-1E55-FA57-1E55-FA571E55FA57" // UUID 
 
-constexpr int LEFT_PIN = 5;
-constexpr int RIGHT_PIN = 3;
-constexpr int LED_PIN = 8;
+static constexpr int LEFT_PIN = 5;
+static constexpr int RIGHT_PIN = 3;
+static constexpr int LED_PIN = 8;
 
-constexpr int LEFT_SENSOR = 0;
-constexpr int RIGHT_SENSOR = 1;
+static constexpr int LEFT_SENSOR = 2;
+static constexpr int RIGHT_SENSOR = 10;
 
-constexpr int TURN_DELAY_MS = 200;
+static constexpr int TURN_DELAY_MS = 200;
+
+static BLECharacteristic *goCharacteristic;
+static BLECharacteristic *stopCharacteristic;
+
+static bool moving = false;
+
+class MyServerCallbacks: public BLEServerCallbacks, public BLECharacteristicCallbacks {
+    void onConnect(BLEServer* pServer) {
+      Serial.println("Client Connected");
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      Serial.println("Client Disconnected");
+      BLEDevice::startAdvertising();
+    }
+    void onWrite(BLECharacteristic *c) {
+      uint8_t value = c->getData()[0];
+      char buf[4];
+      snprintf(buf, sizeof(buf), "%u", value);
+      if (c == goCharacteristic) {
+        moving = true;
+      } else if (c == stopCharacteristic) {
+        moving = false;
+      }
+      Serial.print(buf);
+      Serial.print("\n");
+    }
+};
 
 void setup() {
   Serial.begin(115200);
@@ -31,6 +59,22 @@ void setup() {
   digitalWrite(LED_PIN, LOW);
 
   Serial.println("Peto™");
+  BLEDevice::init("Petobot™");
+  BLEServer *pServer = BLEDevice::createServer();
+
+  pServer->setCallbacks(&cbs);
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  goCharacteristic = pService->createCharacteristic(
+                       CHARACTERISTIC_UUID_GO,
+                       BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY
+                     );
+  goCharacteristic->setCallbacks(&cbs);
+  stopCharacteristic = pService->createCharacteristic(
+                         CHARACTERISTIC_UUID_STOP,
+                         BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY
+                       );
+  stopCharacteristic->setCallbacks(&cbs);
   spettacolino();
   Serial.println("Peto2™");
 }
@@ -50,11 +94,9 @@ void spettacolino() {
   }
 }
 
-void loop() {
-  Serial.println("Peto!™");
+void move() {
   int leftSensor = digitalRead(LEFT_SENSOR);
   int rightSensor = digitalRead(RIGHT_SENSOR);
-
   bool leftDetected = leftSensor == LOW;
   bool rightDetected = rightSensor == LOW;
 
@@ -73,7 +115,13 @@ void loop() {
     // Both sensors on line - stop
     setMotors(false, false);
   }
+}
 
+void loop() {
+  Serial.println("Peto!™");
+  if (moving) {
+    move();
+  }
   delay(50);  // debounce
 }
 
