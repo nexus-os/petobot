d9084d4efd60321faa43ed439211d332791f313a
Simone Baratta <Conte91@gmail.com>
2024-12-28 01:22:13 +0100

    Beautify very much

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -8,88 +8,50 @@
 #define CHARACTERISTIC_UUID_LEFT  "1EF71EF7-1EF7-1EF7-1EF7-1EF71EF71EF7" // UUID for X
 #define CHARACTERISTIC_UUID_RIGHT  "1E551EF7-1E55-1E55-1E55-1E551E551EF7" // UUID for Y
 
+constexpr int LEFT_PIN = 5;
+constexpr int RIGHT_PIN = 3;
+constexpr int LED_PIN = 8;
 
-int leftPin = 5;
-int rightPin = 3;
-int ledPin = 8;
+constexpr int LEFT_SENSOR = 0;
+constexpr int RIGHT_SENSOR = 1;
 
-const int LEFT_SENSOR = A0;
-const int RIGHT_SENSOR = A1;
-
-const int TURN_DELAY_MS = 200;
-
-BLECharacteristic *leftCharacteristic;
-BLECharacteristic *rightCharacteristic;
-
-class MyServerCallbacks: public BLEServerCallbacks, public BLECharacteristicCallbacks {
-    void onConnect(BLEServer* pServer) {
-      Serial.println("Client Connected");
-    };
-
-    void onDisconnect(BLEServer* pServer) {
-      Serial.println("Client Disconnected");
-      BLEDevice::startAdvertising();
-    }
-    void onWrite(BLECharacteristic *c) {
-      uint8_t value = c->getData()[0];
-      char buf[4];
-      snprintf(buf, sizeof(buf), "%u", value);
-      if (c == leftCharacteristic) {
-        analogWrite(leftPin, value);
-      } else if (c == rightCharacteristic) {
-        analogWrite(rightPin, value);
-      }
-      Serial.print(buf);
-      Serial.print("\n");
-    }
-};
-
-MyServerCallbacks cbs;
+constexpr int TURN_DELAY_MS = 200;
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(leftPin, OUTPUT);
-  pinMode(rightPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(LEFT_PIN, OUTPUT);
+  pinMode(RIGHT_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   pinMode(LEFT_SENSOR, INPUT_PULLUP);
   pinMode(RIGHT_SENSOR, INPUT_PULLUP);
 
-  analogWrite(leftPin, 0);
-  analogWrite(rightPin, 0);
-  analogWrite(ledPin, 0);
+  digitalWrite(LEFT_PIN, LOW);
+  digitalWrite(RIGHT_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
 
-  BLEDevice::init("Petobot™");
-  BLEServer *pServer = BLEDevice::createServer();
-
-  pServer->setCallbacks(&cbs);
-
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  leftCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID_LEFT,
-      BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY
-  );
-  leftCharacteristic->setCallbacks(&cbs);
-  rightCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID_RIGHT,
-      BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY
-  );
-  rightCharacteristic->setCallbacks(&cbs);
-
-  pService->start();
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->start();
+  Serial.println("Peto™");
+  spettacolino();
+  Serial.println("Peto2™");
 }
 
 void setMotors(bool left, bool right) {
-  analogWrite(leftPin, left ? 255 : 0);
-  analogWrite(rightPin, right ? 255 : 0);
+  digitalWrite(LEFT_PIN, left ? HIGH : LOW);
+  digitalWrite(RIGHT_PIN, right ? HIGH : LOW);
   Serial.printf("Motors L:%d R:%d\n", left, right);
 }
 
+void spettacolino() {
+  for (int i = 0; i < 5; ++i) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(250);
+    digitalWrite(LED_PIN, LOW);
+    delay(250);
+  }
+}
+
 void loop() {
-  Serial.println("Peto™");
+  Serial.println("Peto!™");
   int leftSensor = digitalRead(LEFT_SENSOR);
   int rightSensor = digitalRead(RIGHT_SENSOR);
 
@@ -114,3 +76,5 @@ void loop() {
 
   delay(50);  // debounce
 }
+
+// vim: set et ts=2 sw=2:
